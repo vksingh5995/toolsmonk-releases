906a28d2e0a8c456f2336aa14a5e1c376aa86c50
Vivek Kumar <81865022+vksingh5995@users.noreply.github.com>
2026-09-21 08:32:44 +0530

    chore(choco): publish 1.1.19

--- choco/toolsmonk/tools/chocolateyinstall.ps1
+++ choco/toolsmonk/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'toolsmonk'
-$url64       = 'https://github.com/vksingh5995/toolsmonk-releases/releases/download/v1.1.18/ToolsMonk-Setup-1.1.18.exe'
-$checksum64  = '82AE3D2725F4135BB536FC8EEBE6D1535CBA080368E340B7C012B9F952BCA200'
+$url64       = 'https://github.com/vksingh5995/toolsmonk-releases/releases/download/v1.1.19/ToolsMonk-Setup-1.1.19.exe'
+$checksum64  = 'C2FD2F2E7AA3AA589EE961B4042C2C80494A32C262CFCE5F514E9E38A7FA7D5F'
 
 # The upstream NSIS installer is per-user by default (electron-builder
 # `perMachine: false`). Chocolatey runs elevated and users expect a machine-wide
